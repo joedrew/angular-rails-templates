d465922505be819c3dafa952bc95a94c6ae35871
Jeremy Ebler <jebler@gmail.com>
2014-04-23 20:47:30 -0700

    AngularRailsTemplates::Template has_a tilt template, not is_a template - WIP - stable

--- test/precompile_test.rb
+++ test/precompile_test.rb
@@ -12,14 +12,15 @@ class PrecompileTest < TestCase
 
   def delete_assets!
     FileUtils.rm_rf app_path.join('public', 'assets')
+    FileUtils.rm_rf app_path.join('tmp', 'cache','assets')
   end
 
   def precompile!(rails_env)
-    ENV['RAILS_ENV'] = rails_env
+    # ENV['RAILS_ENV'] = rails_env
 
-    quietly do
-      Dir.chdir(app_path) { `bundle exec rake assets:precompile` }
-    end
+    # quietly do
+      Dir.chdir(app_path) { `RAILS_ENV=#{rails_env} bundle exec rake assets:precompile` }
+    # end
 
     appjs = Dir["#{app_path}/public/assets/application*.js"].first
 
@@ -36,6 +37,7 @@ class PrecompileTest < TestCase
     assert_match /\.put\("haml_template\.html",/, contents
     assert_match /\.put\("subfolder\/slim_template\.html",/, contents
     assert_match /\.put\("subfolder\/haml_template\.html",/, contents
+    assert_match "ajs-haml", contents
 
     # Check that we render slim templates
     unescaped = contents.gsub(/\\u([\da-fA-F]{4})/) {|m| [$1].pack("H*").unpack("n*").pack("U*")}
@@ -52,14 +54,14 @@ class PrecompileTest < TestCase
   end
 
   def test_precompile_succeeds_in_development_environment
-    precompile! nil
+    precompile! 'development'
   end
 
-  def test_precompile_succeeds_in_production_environment
-    precompile! 'production'
-  end
+  # def test_precompile_succeeds_in_production_environment
+  #   precompile! 'production'
+  # end
 
-  def test_precompile_succeeds_in_test_environment
-    precompile! 'test'
-  end
+  # def test_precompile_succeeds_in_test_environment
+  #   precompile! 'test'
+  # end
 end
