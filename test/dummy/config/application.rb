29a2adf2359b1595319c78c743e8c4dbd47fea5e
Jeremy Ebler <jebler@gmail.com>
2014-07-01 08:24:34 -0700

    test sprockets

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,11 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "rails/test_unit/railtie"
-begin
-  require 'sprockets/rails'
-rescue Exception
-  require "sprockets/railtie"
-end
+# begin
+#   require 'sprockets/rails'
+# rescue Exception
+#   require "sprockets/railtie"
+# end
 
 Bundler.require(*Rails.groups)
 require "angular-rails-templates"
